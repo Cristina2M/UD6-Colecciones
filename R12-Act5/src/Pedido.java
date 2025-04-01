0840dc25b55c86cda2c14b5d0a34b98e0635d5a4
Cristina2M <cmormar2509@g.educaand.es>
2025-04-01 09:11:11 +0000

    Act5-Comentarios

--- R12-Act5/src/Pedido.java
+++ R12-Act5/src/Pedido.java
@@ -1,11 +1,13 @@
 import java.util.HashSet;
 
 public class Pedido {
+    // ATRIBUTOS
     private static int numeroPedidos = 0;
     private int numeroPedido;
     private TipoComida tipoComida;
     private HashSet<String> ingredientesAdicionales;
 
+    // CONSTRUCTOR
     public Pedido(TipoComida tipoComida) {
         numeroPedidos++;
         this.numeroPedido = numeroPedidos;
@@ -13,20 +15,23 @@ public class Pedido {
         this.ingredientesAdicionales = new HashSet<>();
     }
 
+    //MÉTODOS
     public void agregarIngrediente(String ingrediente) {
-        this.ingredientesAdicionales.add(ingrediente);
+        this.ingredientesAdicionales.add(ingrediente);// se van añadiendo los ingredientes
     }
+
 
     public void mostrarPedido() {
         System.out.println("Pedido: " + tipoComida);
         System.out.println("ID del pedido: " + numeroPedido);
-        if (ingredientesAdicionales.isEmpty()) {
+        if (ingredientesAdicionales.isEmpty()) { // comprobamos si está vacío
             System.out.println("Ingredientes extras: Ninguno");
-        } else {
+        } else { // si contiene valores, los muestra
             System.out.println("Ingredientes extras: " + ingredientesAdicionales);
         }
-        System.out.println(); // Salto de línea adicional
+        System.out.println(); // Para añadir un salto de línea
     }
+
 
     public static int getNumeroPedidos() {
         return numeroPedidos;
